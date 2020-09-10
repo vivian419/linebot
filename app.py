6630f08db2acf5c3dfc8075df8e447832a41fda3
vivian419 <71064355+vivian419@users.noreply.github.com>
2020-09-10 16:39:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ import time
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
-line_bot_api = LineBotApi('R60lcR7k2nfcCzSfkRpAFkv6DoiXwJLIBf+zdR8qBfRig60rEZrAoTbWd3pLvmuwJ99ko49MPfPOSuxFy2a/ztkgz7UTbnSFb9BHMKR9viDEVCYirsPhufBz3EE6jllolMzE5DE2wMqKNWP7Xui1vQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('1ylelK+2bj/wnvWcSgcw5DFYtH+IfCzCPYGriuFuua4GCCGxZl+N2zNI6mHCKvoW/lYaciC4EoYNVagvy96dA57CaYGQGwWZIn+kjV0+EyBA/ZuW9dxTA7pgSGaPSRAmLVOUVWS5KATkYWJOr8Zl+QdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('55efd2ee0104755237f28a9567334f3b')
+handler = WebhookHandler('a3b3bfe6d8c91699c8bfb1badae280beb')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
